dda7e2c527baa38c2a0dc458ce8c1771801a96c7
Bryan Tempini <mistatempini@gmail.com>
2022-03-11 11:55:44 -0700

    working generator

--- script.js
+++ script.js
@@ -25,29 +25,33 @@ function generatePassword() {
   var symbol = confirm("Would you like to include symbols?")
   
   if (upperCase) { 
-    passwordCharacters.push = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q","R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    passwordCharacters.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q","R", "S", "T", "U", "V", "W", "X", "Y", "Z")
     console.log(upperCase);
   }
   if (lowerCase) {
-    passwordCharacters.push = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    passwordCharacters.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
     console.log(lowerCase);
   }
   if (number) {
-    passwordCharacters.push = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    passwordCharacters.push("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
     console.log(number);
   }
   if (symbol) {
-    passwordCharacters.push = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "~", "<", ">", ".", ",", ";", ":"]
+    passwordCharacters.push("!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+", "~", "<", ">", ".", ",", ";", ":")
     console.log(symbol);
   }
   
   console.log(passwordCharacters)
 
-  for (let i=0; i < characterLimit; i++) {
-    var index = math.floor(math.random() * passwordCharacters.length);
-  }
-}
+  var finalPassword = "";
 
+  for (let i=0; i < characterLimit; i++) {
+    var char = passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+  finalPassword = finalPassword.concat(char);
+  }
+
+  return finalPassword;
+}
 
 
 

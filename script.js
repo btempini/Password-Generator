82f5a85ec95eaa4dbc520e0872f219b73712ce51
Bryan Tempini <mistatempini@gmail.com>
2022-10-27 20:03:14 -0400

    return password and execute

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function generatePassword() {
   var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
   var upperCaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   var numbersArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-  var specialArr = ['!', '@', '#', '$', '*', '%', '❤️']
+  var specialArr = ['!', '@', '#', '$', '*', '%'] 
 
   //prompts for password criteria
   var passwordLength = prompt('Please choose the length of your password, between 8-128)');
@@ -37,7 +37,15 @@ function generatePassword() {
     }
     console.log(newPasswordCharacters)
   }
-  
+  var newPassword = []
+  //take password length and create a new array based on user input
+  for (var i=0; i < passwordLength; i++) {
+    var random = Math.floor(Math.random() * newPasswordCharacters.length);
+    newPassword = newPassword.concat(newPasswordCharacters[random])
+  }
+  newPassword = newPassword.join("");
+  console.log(newPassword);
+  return(newPassword);
 }
 
 
